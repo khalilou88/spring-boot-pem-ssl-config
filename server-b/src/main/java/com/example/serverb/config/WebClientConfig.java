eb163f4c893c5c2dd55803fbe4d7d1f85f0d0eb0
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-08-04 18:01:04 +0200

    wip

--- server-b/src/main/java/com/example/serverb/config/WebClientConfig.java
+++ server-b/src/main/java/com/example/serverb/config/WebClientConfig.java
@@ -1,28 +1,15 @@
 package com.example.serverb.config;
 
-import org.springframework.boot.ssl.SslBundle;
-import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientSsl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-
-import javax.net.ssl.SSLContext;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(SslBundles sslBundles) {
-        SslBundle sslBundle = sslBundles.getBundle("client");
-        SSLContext sslContext = sslBundle.createSslContext();
-
-        HttpClient httpClient = HttpClient.create()
-                .secure(sslSpec -> sslSpec.sslContext(sslContext));
-
-        return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .build();
+    public WebClient webClient(WebClient.Builder webClientBuilder, WebClientSsl ssl) {
+        return webClientBuilder.apply(ssl.fromBundle("client")).build();
     }
 }
